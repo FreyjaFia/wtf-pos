1aa87564551dd81ac6b2da51d2bae26cee39edb0
Alain Elemia <alain.elemia@gmail.com>
2026-02-06 15:44:17 +0800

    Add Home feature (template, logic, styles, spec)

---
+++ src/app/features/home/home.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  private readonly router = inject(Router);
+  private readonly auth = inject(AuthService);
+
+  logout() {
+    this.auth.logout();
+    this.router.navigateByUrl('/login');
+  }
+}
