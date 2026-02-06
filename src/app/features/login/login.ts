845aeb595ad8be68c4139fb04bc6d38ada87d5f4
Alain Elemia <alain.elemia@gmail.com>
2026-02-06 15:43:32 +0800

    Add Login feature (template, logic, styles, spec)

---
+++ src/app/features/login/login.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  private readonly router = inject(Router);
+  private readonly auth = inject(AuthService);
+
+  protected loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  login() {
+    // TODO: Replace this with real authentication call
+    // Example: await authApi.signIn(username, password)
+    this.auth.login();
+    this.router.navigateByUrl('/home');
+  }
+}
