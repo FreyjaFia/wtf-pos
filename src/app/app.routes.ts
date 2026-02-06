7fbab8cdde5bdd3d52ee66e07ca0b5725f5db325
Alain Elemia <alain.elemia@gmail.com>
2026-02-06 15:43:07 +0800

    Update app shell and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Login } from './features/login/login';
+import { Test } from './features/test/test';
+import { authGuard } from './guards/auth-guard';
+import { Layout } from './shared/layout/layout';
+import { Orders } from './features/orders/orders';
+import { NewOrder } from './features/orders/new-order/new-order';
+import { OrderList } from './features/orders/order-list/order-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: '',
+    component: Layout,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'home',
+        component: Home,
+      },
+      {
+        path: 'test',
+        component: Test,
+      },
+      {
+        path: 'orders',
+        component: Orders,
+        children: [
+          { path: 'new', component: NewOrder },
+          { path: 'list', component: OrderList },
+        ],
+      },
+    ],
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IconsSprite } from './shared/icons/icons-sprite/icons-sprite';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IconsSprite],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('wtf-pos');
