0234b14402c92aa53bc62f0e97712e41d43c0133
Alain Elemia <alain.elemia@gmail.com>
2026-02-16 07:05:52 +0800

    Export AlertService and GlobalAlertComponent
    Add exports to service and component index files to make
    new alert infrastructure available throughout the app.

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -3,6 +3,7 @@ export * from './alert/alert';
 export * from './badge/badge';
 export * from './dock/dock';
 export * from './filter-dropdown/filter-dropdown';
+export * from './global-alert/global-alert';
 export * from './header/header';
 export * from './icons/icon/icon';
 export * from './icons/icons-sprite/icons-sprite';
